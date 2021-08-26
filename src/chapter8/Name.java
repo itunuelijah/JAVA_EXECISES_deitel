71370b210501aaf5bb74210846b95efe36790aee
itunuelijah <itunuelijah@gmail.com>
2021-08-26 13:46:32 +0100

    reversed name

--- src/chapter8/Name.java
+++ src/chapter8/Name.java
@@ -11,8 +11,11 @@ public class Name{
 
         System.out.println("Reversed name: ");
 
+
         for(int i = name.length() - 1; i >= 0; i--) {
         System.out.print(name.charAt(i) + " ");
         }
     }
+
+
 }
--- src/chapter8/PackageDataTest.java
+++ src/chapter8/PackageDataTest.java
@@ -1,8 +1,8 @@
 package chapter8;
-
+/**
 public class PackageDataTest {
     public static void main(String[] args) {
         PackageData packageData = new PackageData();
         System.out.printf("After instantiation:%n%s%n", packageData);
     }
-}
+}**/
