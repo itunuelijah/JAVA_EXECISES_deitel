f12396c649957751903d191d3218530969523359
itunuelijah <itunuelijah@gmail.com>
2021-09-29 11:31:21 +0100

    update

--- src/Semicolon_Execise/DisplayArraysByHarsh.java
+++ src/Semicolon_Execise/DisplayArraysByHarsh.java
@@ -7,11 +7,11 @@ public class DisplayArraysByHarsh {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 4; j++) {
                 if (array[i][j] == 0){
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
 
                 else{
-                    System.out.print("#");
+                    System.out.print("# ");
                 }
 
                 //System.out.print(array[i][j]);
