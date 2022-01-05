768bbf2e1a1ea4549ef7cec7c870024ba5138743
itunuelijah <itunuelijah@gmail.com>
2022-01-05 23:25:14 +0100

    kate update

--- test/chapter5/KataTest.java
+++ test/chapter5/KataTest.java
@@ -11,46 +11,52 @@ class KataTest {
     private int copiesOfPastQuestions;
 
     @Test
-    public void testPriceForLessThan5(){
+    public void testPriceForLessThan5() {
         Kata kata = new Kata();
         int totalPrice = kata.getPrice(1);
         Assertions.assertEquals(1500, totalPrice);
     }
 
     @Test
-    public void testPriceForLessThan10(){
+    public void testPriceForLessThan10() {
         Kata kata = new Kata();
         int totalPrice = kata.getPrice(6);
         Assertions.assertEquals(1400, totalPrice);
     }
+
     @Test
-    public void testPriceForLessThan30(){
+    public void testPriceForLessThan30() {
         Kata kata = new Kata();
         int totalPrice = kata.getPrice(12);
         Assertions.assertEquals(1200, totalPrice);
     }
+
     @Test
-    public void testPriceForLessThan50(){
+    public void testPriceForLessThan50() {
         Kata kata = new Kata();
         int totalPrice = kata.getPrice(34);
         Assertions.assertEquals(1100, totalPrice);
     }
+
     @Test
-    public void testPriceForLessThan100(){
+    public void testPriceForLessThan100() {
         Kata kata = new Kata();
         int totalPrice = kata.getPrice(57);
         Assertions.assertEquals(1000, totalPrice);
     }
+
     @Test
-    public void testPriceForLessThan200(){
+    public void testPriceForLessThan200() {
         Kata kata = new Kata();
         int totalPrice = kata.getPrice(148);
         Assertions.assertEquals(900, totalPrice);
     }
+
     @Test
-    public void testPriceForGreaterThan100(){
+    public void testPriceForGreaterThan100() {
         Kata kata = new Kata();
         int totalPrice = kata.getPrice(257);
         Assertions.assertEquals(800, totalPrice);
     }
 
+}
