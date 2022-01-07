d51d4a096f340eb360396b8b9b53eb14d1480186
itunuelijah <itunuelijah@gmail.com>
2022-01-07 05:15:16 +0100

    update for account test

---
+++ src/chapter3/AccountTest.java
@@ -0,0 +1,12 @@
+package chapter3;
+
+
+
+import java.util.Scanner;
+
+public class AccountTest {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
